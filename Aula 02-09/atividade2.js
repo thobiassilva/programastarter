cd1cb9b5c3a3100fba96986c61a2515d4849b2ba
Thobias Silva <thobiaspsilva@yahoo.com.br>
2021-09-07 12:56:51 -0300

    Atividade 2 sobre DOM

---
+++ Aula 02-09/atividade2.js
@@ -0,0 +1,7 @@
+function changeTitle() {
+  document.getElementById('title').innerText = 'GROWDEV - Programa Starter 5ed';
+}
+
+function hide() {
+  document.getElementById('profilePhoto').style.display = 'none';
+}
