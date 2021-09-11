60567de395de538aa7a1acd4b400c8988a95c9f4
Thobias Silva <thobiaspsilva@yahoo.com.br>
2021-09-11 12:08:01 -0300

    Atividade final modulo I

--- Avaliacao Final Modulo I/script.js
+++ Avaliacao Final Modulo I/script.js
@@ -5,23 +5,33 @@ const pessoa = {
   email: 'thobiaspsilva@gmail.com',
   telefone: '(51) 99687-3769',
   descricao:
-    'Eu sou uma pessoa muito divertida e objetiva, sei o que quero na vida. Adoro tecnologia e programação.',
+    'Olá, atualmente sou programador Trainee em Flutter Mobile e cursando o Programa Starter da Growdev para FullStack. Estou sempre em busca novos conhecimentos e evolução, tento levar a vida de uma forma leve, descontraída e com muita dedicação sempre.',
   redes_sociais: [
     'https://www.linkedin.com/in/thobiaspsilva/',
     'https://github.com/thobiassilva',
     'https://twitter.com/thobiaspsilva',
     'https://www.facebook.com/thobiaspsilva/',
   ],
-  conhecimentos: [],
-  objetivo_pessoal: '',
-  objetivo_profissional: '',
+  conhecimentos: ['Dart', 'Flutter', 'SQL', 'Java Android', 'HTML', 'CSS', 'JavaScript'],
+  objetivo_pessoal:
+    'Tenho como objetivo pessoal estar presente em todas as evoluções da tecnologia bem como ter minhas metas alçadas. ',
+  objetivo_profissional:
+    'Profissionalmente pretendo me tornar alguém bem sucedido na carreira de tecnologia',
   photo: 'https://avatars.githubusercontent.com/u/39360514?v=4',
 };
 
 let content = document.getElementById('content');
+let personInfo = document.getElementById('personInfo');
+let socialLinks = document.getElementById('socialLinks');
 
 function onLoad() {
   document.getElementById('profilePhoto').src = pessoa.photo;
+  about();
+}
+
+function about() {
+  personInfo.style.display = 'block';
+  socialLinks.style.display = 'block';
   document.getElementById('name').innerText = pessoa.nome.toUpperCase();
   document.getElementById('lastName').innerText = pessoa.sobrenome.toUpperCase();
   document.getElementById('andress').innerText = pessoa.endereco.toUpperCase();
@@ -31,16 +41,15 @@ function onLoad() {
   document.getElementById('github').href = pessoa.redes_sociais[1];
   document.getElementById('twitter').href = pessoa.redes_sociais[2];
   document.getElementById('facebook').href = pessoa.redes_sociais[3];
-
-  about();
-}
-
-function about() {
   content.innerText = pessoa.descricao;
 }
 function skills() {
-  content.innerText = pessoa.descricao;
+  personInfo.style.display = 'none';
+  socialLinks.style.display = 'none';
+  content.innerText = pessoa.conhecimentos.join('\n');
 }
 function objetive() {
+  personInfo.style.display = 'none';
+  socialLinks.style.display = 'none';
   content.innerText = `${pessoa.objetivo_pessoal}\n ${pessoa.objetivo_profissional} `;
 }
