8c8c01e74a5c92b0b37efbd0b16e1794614a3a05
Thobias <thobiaspsilva@yahoo.com.br>
2021-09-23 02:34:21 -0300

    Ajusta insert

--- Avaliacao Final Modulo II/home/homeController.js
+++ Avaliacao Final Modulo II/home/homeController.js
@@ -9,31 +9,50 @@ class StorageAdapter {
         localStorage.removeItem(key);
     }
 }
-let messagesTable = document.getElementById('messagesTable');
 
-function onLoad() {
-    const username = sessionStorage.getItem('user');
-    let users = StorageAdapter.getItem('users');
-    let messages = [];
 
-    for (const user of users) {
-        if (username == user.username) {
-            messages = user.messages;
-            break;
-        }
-    }
+var messagesTable = document.getElementById('messagesTable').getElementsByTagName('tbody')[0];
+const username = sessionStorage.getItem('user');
+let users = StorageAdapter.getItem('users');
+let user;
+let userUid;
 
-    for (let i = 0; i < messages.lenght; i++) {
-        messagesTable.insertRow(i).insertCell(i).innerHTML = 'NEW CELL';
+for (let i = 0; i < users.length; i++) {
+    if (username == users[i].username) {
+        user = users[i];
+        userUid = i;
+        break;
     }
 }
 
-
+for (let x = 0; x < user.messages.length; x++) {
+    console.log(user.messages[x]);
+    let row = messagesTable.insertRow(messagesTable.rows.length);
+    row.insertCell().innerHTML = x + 1;
+    row.insertCell().innerHTML = user.messages[x].description;
+    row.insertCell().innerHTML = user.messages[x].detail;
+    row.insertCell().innerHTML = '<button type="button" class="btn btn-danger text-light me-2" data-bs-toggle="modal" data-bs-target="#deleteModal">Apagar</button><button type="button" class="btn btn-success text-light" data-bs-toggle="modal" data-bs-target="#editModal">Editar</button>';
+}
 
 function insert() {
     let description = document.getElementById('description').value;
     let detail = document.getElementById('detail').value;
 
+    user.messages.push(new Message(description, detail));
 
+    users[userUid] = user;
+
+    StorageAdapter.setItem('users', users);
+
+    let row = messagesTable.insertRow(messagesTable.rows.length);
+    row.insertCell().innerHTML = messagesTable.rows.length;
+    row.insertCell().innerHTML = description;
+    row.insertCell().innerHTML = detail;
+    row.insertCell().innerHTML = '<button type="button" class="btn btn-danger text-light me-2" data-bs-toggle="modal" data-bs-target="#deleteModal">Apagar</button><button type="button" class="btn btn-success text-light" data-bs-toggle="modal" data-bs-target="#editModal">Editar</button>';
+}
+
+function Message(_description, _detail) {
+    this.description = _description;
+    this.detail = _detail;
 }
 
